3b7734948bf4fac2f3ca16143c0bcae74266de98
TestSubjectUser <wami50731@gmail.com>
2025-03-11 18:32:13 +0530

    input component to edit title, description

--- components/create-your-comp/Step.tsx
+++ components/create-your-comp/Step.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BlinkingBubble } from "./BlinkingBubble";
 import EditableHeader from "./EditableHeader";
+import AddComp from "./AddComp";
 
 interface StepProps {
   step: any;
@@ -12,9 +13,22 @@ interface StepProps {
   index: number;
   imageRefs: any;
   updateStep: (index: number, newTitle: string, newDescription: string) => void;
+  addStep: (
+    index: number,
+    newTitle: string,
+    newDescription: string,
+    screenshotUrl: string,
+    relativeCoordinates: { x: number; y: number }
+  ) => void;
 }
 
-const Step: React.FC<StepProps> = ({ step, index, imageRefs, updateStep }) => {
+const Step: React.FC<StepProps> = ({
+  step,
+  index,
+  imageRefs,
+  updateStep,
+  addStep,
+}) => {
   const [imageLoaded, setImageLoaded] = React.useState(false);
 
   React.useEffect(() => {
@@ -25,52 +39,56 @@ const Step: React.FC<StepProps> = ({ step, index, imageRefs, updateStep }) => {
   }, [imageRefs.current[index]]);
 
   return (
-    <div className="step" id={index.toString()} key={index}>
-      <div className="step-header">
-        <div className="step-number">{index + 1}</div>
-        <EditableHeader
-          textValue={step.title}
-          textColor=""
-          textSize=""
-          setText={(newText) => updateStep(index, newText, step.description)}
-        />
-      </div>
-      <div className="step-description">
-        <EditableHeader
-          textValue={step.description}
-          textColor="gray"
-          textSize=""
-          setText={(newText) => updateStep(index, step.title, newText)}
-        />
-      </div>
-
-      <div
-        className="image-container"
-        style={{ position: "relative", overflow: "hidden" }}
-      >
-        <img
-          ref={(el) => (imageRefs.current[index] = el)}
-          src={step.screenshotUrl}
-          alt={step.id}
-          className="step-image"
-          onLoad={() => setImageLoaded(true)}
-          style={{
-            maxHeight: "600px",
-            maxWidth: "100%",
-            objectFit: "contain",
-          }}
-        />
-        {imageLoaded && (
-          <BlinkingBubble
-            coordinates={{
-              x: step.relativeCoordinates.x,
-              y: step.relativeCoordinates.y,
-            }}
-            imageRef={imageRefs.current[index]}
+    <>
+      {index === 0 && <AddComp index={0} addStep={addStep} />}
+      <div className="step" id={index.toString()} key={index}>
+        <div className="step-header">
+          <div className="step-number">{index + 1}</div>
+          <EditableHeader
+            textValue={step.title}
+            textColor=""
+            textSize=""
+            setText={(newText) => updateStep(index, newText, step.description)}
           />
-        )}
+        </div>
+        <div className="step-description">
+          <EditableHeader
+            textValue={step.description}
+            textColor="gray"
+            textSize=""
+            setText={(newText) => updateStep(index, step.title, newText)}
+          />
+        </div>
+
+        <div
+          className="image-container"
+          style={{ position: "relative", overflow: "hidden" }}
+        >
+          <img
+            ref={(el) => (imageRefs.current[index] = el)}
+            src={step.screenshotUrl}
+            alt={step.id}
+            className="step-image"
+            onLoad={() => setImageLoaded(true)}
+            style={{
+              maxHeight: "600px",
+              maxWidth: "100%",
+              objectFit: "contain",
+            }}
+          />
+          {imageLoaded && (
+            <BlinkingBubble
+              coordinates={{
+                x: step.relativeCoordinates.x,
+                y: step.relativeCoordinates.y,
+              }}
+              imageRef={imageRefs.current[index]}
+            />
+          )}
+        </div>
       </div>
-    </div>
+      <AddComp index={index + 1} addStep={addStep} />
+    </>
   );
 };
 
